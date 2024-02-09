98e7e14feb3ab426ab99afa99788ae04568e2509
darsh9510 <118364960+darsh9510@users.noreply.github.com>
2024-02-09 22:10:35 +0530

    Add files via upload

---
+++ base/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+class Massage(models.Model):
+    user = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+    text = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def _str_(self):
+        return self.text
